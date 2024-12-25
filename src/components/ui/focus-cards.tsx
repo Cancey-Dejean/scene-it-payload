67dcb709105fffbd9daed0e4a3e98bb8dba2f2c4
Cancey-Dejean <canceyd@gmail.com>
2024-12-25 12:42:59 -0600

    Refactor FocusCards component for improved code clarity and consistency
    - Standardized import statements and removed unnecessary quotes for cleaner syntax.
    - Simplified prop types and class names for better readability.
    - Enhanced hover effects and transition properties for a smoother user experience.
    - Cleaned up component structure to improve maintainability.
    
    This commit enhances the FocusCards component by streamlining the code and improving the overall UI interaction.

--- src/components/ui/focus-cards.tsx
+++ src/components/ui/focus-cards.tsx
@@ -1,10 +1,10 @@
-"use client";
-import Image from "next/image";
-import React, { useState } from "react";
-import { cn } from "@/lib/utils";
-import { Movie } from "@/types";
-import { imageBaseUrl } from "@/constants";
-import Link from "next/link";
+'use client'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { cn } from '@/lib/utils'
+import { Movie } from '@/types'
+import { imageBaseUrl } from '@/constants'
+import Link from 'next/link'
 
 export const Card = React.memo(
   ({
@@ -13,47 +13,44 @@ export const Card = React.memo(
     hovered,
     setHovered,
   }: {
-    card: Movie;
-    index: number;
-    hovered: number | null;
-    setHovered: React.Dispatch<React.SetStateAction<number | null>>;
+    card: Movie
+    index: number
+    hovered: number | null
+    setHovered: React.Dispatch<React.SetStateAction<number | null>>
   }) => (
     <div
       onMouseEnter={() => setHovered(index)}
       onMouseLeave={() => setHovered(null)}
       className={cn(
-        "relative h-[352px] w-full overflow-hidden rounded-lg bg-gray-100 transition-all duration-300 ease-out dark:bg-neutral-900",
-        hovered !== null && hovered !== index && "scale-[0.98] blur-sm",
+        'relative h-[352px] w-full overflow-hidden rounded-lg  transition-all duration-300 ease-out dark:bg-neutral-900',
+        hovered !== null && hovered !== index && 'scale-[0.98] blur-sm',
       )}
     >
       <Image
         src={`${imageBaseUrl}${card.poster_path}`}
-        alt={card.title || ""}
+        alt={card.title || ''}
         fill
         className="absolute inset-0 object-cover"
       />
       <div
         className={cn(
-          "absolute inset-0 flex items-end bg-black/50 px-4 py-8 transition-opacity duration-300",
-          hovered === index ? "opacity-100" : "opacity-0",
+          'absolute inset-0 flex items-end bg-black/50 px-4 py-8 transition-opacity duration-300',
+          hovered === index ? 'opacity-100' : 'opacity-0',
         )}
       >
         <div className="bg-gradient-to-b from-neutral-50 to-neutral-200 bg-clip-text text-xl font-medium text-transparent md:text-2xl">
           {card.title}
         </div>
       </div>
-      <Link
-        href={`/movies/${card.id}`}
-        className="after:absolute after:inset-0"
-      />
+      <Link href={`/movies/${card.id}`} className="after:absolute after:inset-0" />
     </div>
   ),
-);
+)
 
-Card.displayName = "Card";
+Card.displayName = 'Card'
 
 export function FocusCards({ cards }: { cards: Movie[] }) {
-  const [hovered, setHovered] = useState<number | null>(null);
+  const [hovered, setHovered] = useState<number | null>(null)
 
   return (
     <>
@@ -67,5 +64,5 @@ export function FocusCards({ cards }: { cards: Movie[] }) {
         />
       ))}
     </>
-  );
+  )
 }
